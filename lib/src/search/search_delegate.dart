625f4ba48d865561b8120beb16dff6d6a7e45a0d
Asalmerontkd <psrantonio@gmail.com>
2020-07-30 23:07:56 -0500

    Search delegate, se crea clase generica de busqueda

---
+++ lib/src/search/search_delegate.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class DataSearch extends SearchDelegate{
+  @override
+  List<Widget> buildActions(BuildContext context) {
+      // LAS ACCIONES DE NUESTRO APP BAR
+      throw UnimplementedError();
+    }
+  
+    @override
+    Widget buildLeading(BuildContext context) {
+      // ICONO A LA IZQUIERDA DEL APPBAR
+      throw UnimplementedError();
+    }
+  
+    @override
+    Widget buildResults(BuildContext context) {
+      // CREA LOS RESULTADOS QUE SE VAN A MOSTRAR
+      throw UnimplementedError();
+    }
+  
+    @override
+    Widget buildSuggestions(BuildContext context) {
+    // SON LAS SUGERENCIAS CUANDO LA PERSONA ESCRIBE
+    throw UnimplementedError();
+  }
+
+}
